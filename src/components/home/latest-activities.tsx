2198ba369c9ace948051c974a52b166e7ea64c32
Herbert Ntim <herbertntim15@gmail.com>
2024-01-30 05:57:25 +0000

    rendering latest activities

--- src/components/home/latest-activities.tsx
+++ src/components/home/latest-activities.tsx
@@ -1,8 +1,43 @@
+import { Card, List } from 'antd'
 import React from 'react'
+import { Text } from '../text'
+import { UnorderedListOutlined } from '@ant-design/icons'
+import LatestActivitiesSkeleton from '../skeleton/latest-activities'
 
 const LatestActivities = () => {
+  const isLoading = true
+
   return (
-    <div>LatestActivities</div>
+    <Card
+      bodyStyle={{ padding: '0 1rem' }}
+      headStyle={{ padding: '16px' }}
+      title={(
+        <div 
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            gap: '8px'
+          }}
+        >
+          <UnorderedListOutlined />
+          <Text size='md' style={{ marginLeft: '0.5rem' }}>
+            Latest Activities
+          </Text>
+        </div>
+      )}
+    >
+      {isLoading ? (
+        <List 
+          itemLayout='horizontal'
+          dataSource={Array.from({ length: 5 }).map((_, i) => ({ id: i}))}
+          renderItem={(_, index) => (
+            <LatestActivitiesSkeleton key={index} />
+          )}
+        />
+      ): (
+        <List />
+      )}
+    </Card>
   )
 }
 
